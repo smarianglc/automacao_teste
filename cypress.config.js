64ea054120a87efcb555e600b1ca0a08c3293c27
Maria Angelica <mariancademico@gmail.com>
2023-05-18 16:23:13 -0400

    caso de teste de LOGIN

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '6dk6i6',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl:'https://hom-prod-20.playservicos.com.br/',
+    "reporter": "mochawesome",
+    "reporterOptions":{
+        "reportDir": "cypress/reports/mochawesome-report",
+        "overwrite": true,
+        "html": true,
+        "json": false,
+        "timestamp": "YYYYMMDD_HHmmss"
     },
   },
-});
+})
+  
+
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,129 @@
+describe('LOGIN', () => {
+
+    it('Login com senha errada', () => {
+        cy.visit('/')
+            .get('.flex .h-full')
+        
+        cy.get('.text-caption-lg')
+            .contains('DEMO')    
+
+        //MENU 
+        cy.get('.ph-list-light')
+            .click()
+
+        cy.contains('Entrar')
+            .click()
+
+        //FORMULARIO DE LOGIN
+        cy.get('input[name="cpf"]')
+            .click()
+            .type('02792595140') 
+        
+        cy.get('input[name="senha"]')
+            .click()
+            .type('1618222222')
+
+        cy.get('.v-btn__content')
+            .contains('Entrar')
+            .click()
+
+        //MENSAGEM DE ERRO
+        cy.get('#swal2-content')
+            .should('be.visible')
+            .should('have.text','Usuário ou Senha incorretos')
+        
+        cy.get('.swal2-confirm')
+            .contains('OK')
+            .click()
+
+    });
+    
+    it('Login com cpf errado', () => {
+        cy.visit('/')
+            .get('.flex .h-full')
+
+        cy.get('.text-caption-lg')
+            .contains('DEMO')
+
+        //MENU 
+        cy.get('.ph-list-light')
+            .click()
+
+        cy.contains('Entrar')
+            .click()
+
+        //FORMULARIO DE LOGIN
+        cy.get('input[name="cpf"]')
+            .click()
+            .type('02792432189') 
+
+        cy.get('.v-messages__message')
+            .should('be.visible')
+            .should('have.text','CPF Inválido!')
+
+        cy.get('input[name="senha"]')
+            .click()
+            .type('16184321')
+
+        cy.get('.v-btn__content')
+            .contains('Entrar')
+            .click()
+    });
+
+    it('Login com sucesso', () => {
+        cy.visit('/')
+            .get('.flex .h-full')
+        
+        //MENU
+        cy.get('.text-caption-lg')
+            .contains('DEMO')    
+
+        cy.get('.ph-list-light')
+            .click()
+
+        cy.contains('Entrar')
+            .click()
+
+        //FORMULARIO DE LOGIN
+        cy.get('input[name="cpf"]')
+            .click()
+            .type('02792595140')
+        
+        cy.get('input[name="senha"]')
+            .click()
+            .type('16184321')
+
+        cy.get('.v-btn__content')
+            .contains('Entrar')
+            .click()
+    });
+
+    it.only('Não preencher campos', () => {
+        cy.visit('/')
+            .get('.flex .h-full')
+
+        cy.get('.text-caption-lg')
+            .contains('DEMO')
+
+        //MENU
+        cy.get('.ph-list-light')
+            .click()
+
+        cy.contains('Entrar')
+            .click()
+
+        //FORMULARIO DE LOGIN
+        cy.get('.v-btn__content')
+            .contains('Entrar')
+            .click()
+
+        cy.get('.v-messages__message')
+            .should('be.visible')
+            .should('have.text','Campo obrigatório!')
+
+        cy.get('.v-messages__message')
+            .should('be.visible')
+            .should('have.text','É obrigatório informar a Senha')
+    });
+
+});
