9ce5a8eff117320da00a8bea8a23fb649dc9cb52
Maria Angelica <mariancademico@gmail.com>
2023-05-30 15:18:23 -0400

    comandos personalizados e separando os elementos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,8 +8,7 @@ describe('LOGIN', () => {
         cy.acessarPaginaLogin()
     });
 
-    it.only('Login com senha errada', () => {
-        //FORMULARIO DE LOGIN
+    it('Login com senha errada', () => {
         cy.preencherCPF(user_data.user_cpf)
         cy.preencherSenha(user_data.user_senha_invalida)
         cy.fazerLogin()
@@ -18,35 +17,22 @@ describe('LOGIN', () => {
     });
     
     it('Login com cpf errado', () => {
-        //FORMULARIO DE LOGIN
         cy.preencherCPF(user_data.user_cpf_invalido)
         cy.preencherSenha(user_data.user_senha) 
-
-        cy.get('.v-messages__message')
-            .should('be.visible')
-            .should('have.text','CPF Inválido!')
-
+        cy.mensagemCpfInvalido('CPF Inválido!')
         cy.fazerLogin()
     });
 
     it('Login com sucesso', () => {
-        //FORMULARIO DE LOGIN
         cy.preencherCPF(user_data.user_cpf)
         cy.preencherSenha(user_data.user_senha)
         cy.fazerLogin()
     });
 
     it('Não preencher campos', () => {
-        //FORMULARIO DE LOGIN
         cy.fazerLogin()
-
-        cy.get('.v-messages__message')
-            .should('be.visible')
-            .should('include.text','Campo obrigatório!')
-
-        cy.get('.v-messages__message')
-            .should('be.visible')
-            .should('include.text','É obrigatório informar a Senha')
+        cy.campoObrigatorio('Campo obrigatório!')
+        cy.campoObrigatorio('É obrigatório informar a Senha')
     });
 
 });
--- cypress/support/login_page_commands.js
+++ cypress/support/login_page_commands.js
@@ -1,30 +1,61 @@
 ///<reference types="cypress" />
+//botão
+const botaoEntrar = '.v-btn__content'
+const botaoOkModal = '.swal2-confirm'
+
+//campos de dados
+const campoCPF = 'input[name="cpf"]'
+const campoSenha = 'input[name="senha"]'
+
+//modal
+const modalMensagemErro = '#swal2-content'
+
+//mensagem campo obrigatório
+const mensagemCampoObrigatorio = '.v-messages__message'
+
+//---------------------------------------------------------------
+//ELEMENTOS CLICKS
 Cypress.Commands.add('fazerLogin', () => { 
-    cy.get('.v-btn__content')
+    cy.get(botaoEntrar)
         .contains('Entrar')
         .click()
 });
 
+Cypress.Commands.add('okModalMensagemErro', () => { 
+    cy.get(botaoOkModal)
+        .contains('OK')
+        .click()
+});
+
+//ELEMENTOS TYPE
 Cypress.Commands.add('preencherCPF', (cpf) => {
-    cy.get('input[name="cpf"]')
+    cy.get(campoCPF)
         .click()
         .type(cpf)
 });
 
 Cypress.Commands.add('preencherSenha', (senha) => {
-    cy.get('input[name="senha"]')
+    cy.get(campoSenha)
         .click()
         .type(senha)
 });
 
+//ELEMNTO MODAL
 Cypress.Commands.add('modalMensagemErro', (mensagem) => { 
-    cy.get('#swal2-content')
+    cy.get(modalMensagemErro)
         .should('be.visible')
         .should('have.text',mensagem)
 });
 
-Cypress.Commands.add('okModalMensagemErro', () => { 
-    cy.get('.swal2-confirm')
-        .contains('OK')
-        .click()
+//ELEMENTOS VALIDAÇÕES
+Cypress.Commands.add('mensagemCpfInvalido', (mensagem) => { 
+    cy.get(mensagemCampoObrigatorio)
+        .should('be.visible')
+        .should('have.text',mensagem)
+});
+
+Cypress.Commands.add('campoObrigatorio', (mensagem) => { 
+    cy.get(mensagemCampoObrigatorio)
+        .should('be.visible')
+        .should('include.text',mensagem)
 });
